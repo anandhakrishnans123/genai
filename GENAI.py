b82660bc4303222b514270d4575e0a86ebc28819
anandhakrishnans123 <career.anandhakrishnans@gmail.com>
2024-12-18 17:24:03 +0530

    Update GENAI.py

--- GENAI.py
+++ GENAI.py
@@ -128,6 +128,9 @@ if api_key:
                     df2['Country'] = "India"
                     df2['Energy Type'] = "India"
                     df2["Energy Consumption"] = 2000
+                    df2['End date'] = pd.to_datetime(df2['End date'], format='%m/%d/%Y')
+                    df2['Start Date'] = pd.to_datetime(df2['Start Date'], format='%m/%d/%Y')
+                    df2['Res_Date'] = pd.to_datetime(df2['Res_Date'], format='%m/%d/%Y')
 
                     # Create an in-memory Excel file
                     excel_buffer = io.BytesIO()
